d24102769e330fe50dfdfa7c560ed82b5554c6c5
Brian Ressler <carlofabyss@hotmail.com>
2025-03-04 09:44:55 -0600

    Update with cloudflare source files, for some reason I cannot find the original copy that has receiveMessage functionality

--- index.js
+++ index.js
@@ -1,47 +1,39 @@
-let save_opt = document.getElementById("save_opt");
-let delete_opt = document.getElementById("delete_opt");
-let cookie_input = document.getElementById("cookie_input");
-
-save_opt.addEventListener("click", function() {
-    if(cookie_input.value != null && cookie_input.value != '') {
-        // const d = new Date();
-        // let exdays = 2;
-        // d.setTime(d.getTime() + (exdays * 24*60*60*1000));
-        // let expires = d.toUTCString();
-        localStorage.setItem("savedtext", cookie_input.value);
-    } else {
-        alert("Enter some text to save");
-    }
-
-
-    update_output();
-});
-
-delete_opt.addEventListener("click", function() {
-    localStorage.clear();
-    update_output();
-});
-
-function get_val(value) {
-    let stored_val = localStorage.getItem(value);
-
-    if(stored_val == null || stored_val == "") {
-        return "";
-    } else {
-        return stored_val;
-    }
-}
-
-function update_output() {
+document.addEventListener("DOMContentLoaded", function() {
+    let save_opt = document.getElementById("save_opt");
+    let delete_opt = document.getElementById("delete_opt");
     let cookie_output = document.getElementById("cookie_output");
-    let cookie_val = get_val("savedtext");
+    let cookie_input = document.getElementById("cookie_input");
+    const default_val = 'Not saved...';
+    let cookie_val = default_val;
 
-    if(cookie_val != "") {
+    save_opt.addEventListener("click", function() {
+        if(cookie_input.value != null && cookie_output != null && cookie_input.value != '') {
+            cookie_val = cookie_input.value;
+            cookie_output.innerHTML = cookie_val;
+            window.parent.postMessage(cookie_val, "*");
+        } else {
+            alert("Enter some text to save");
+        }
+
+
+    });
+
+    delete_opt.addEventListener("click", function() {
+        cookie_val = default_val;
         cookie_output.innerHTML = cookie_val;
-    } else {
-        cookie_output.innerHTML = "Not saved...";
-    }
-    
-}
+        window.parent.postMessage(cookie_val, "*");
+    });
 
-update_output();
+    window.addEventListener('message', receiveMessage, false);
+
+    function receiveMessage(event){
+        console.log("Received something from parent: " + event.origin);
+        if(event.origin!=="https://bressler95tops.github.io")
+        return;
+        
+        cookie_val = event.data;
+        cookie_output.innerHTML = cookie_val;
+
+    }
+
+});
